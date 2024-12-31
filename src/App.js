5537c0db11f61fbdbc82e93170f1304e24563b61
kdumbl <kdumblauskas@gmail.com>
2024-12-30 22:29:26 -0600

    Lots

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+ 
 function App() {
+
+let x = getData();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'PP'>
+    <p className = 'PP.text'>
+      yo
+    </p>
     </div>
   );
 }
